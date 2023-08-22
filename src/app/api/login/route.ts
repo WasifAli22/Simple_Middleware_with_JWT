29adffecd15dcb9b7d49ab76ba3fc865c29e8604
Wasif Shahid <wasifaliwasif22@gmail.com>
2023-08-22 11:51:10 +0500

    Simple Middleware with jwt and jose library

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,30 @@
+import { getJWTSecretKey } from "@/lib/auth";
+import { SignJWT } from "jose";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  if (body.username === "admin" && body.password === "admin") {
+    const jwt = await new SignJWT({
+      username: body.username,
+      role: "admin", // Set your own roles
+    })
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .setExpirationTime("3m") // Set your own expiration time
+      .sign(new TextEncoder().encode(getJWTSecretKey()));
+
+    cookies().set("token", jwt, {
+      httpOnly: true,
+    });
+
+    return NextResponse.json({ accessToken: jwt }, { status: 200 });
+  }
+
+  return NextResponse.json(
+    { Error: "Failed to create token" },
+    { status: 400 }
+  );
+}
---
+++ src/app/protected/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Protected = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <h1 className="text-4xl font-bold italic">Protected Page</h1>
+    </div>
+  );
+};
+
+export default Protected;
